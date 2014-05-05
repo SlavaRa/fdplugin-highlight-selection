b15fa7683136fd5460c6bedf2d5641a428b48647
slavara <ratdg@ya.ru>
2014-05-05 20:58:38 +0400

    Added new category of settings: General

--- Settings.cs
+++ Settings.cs
@@ -12,21 +12,25 @@ namespace HighlightSelection
         public const bool DEFAULT_ADD_LINE_MARKER = true;
         public const bool HIGHLIGHT_UNDER_CURSOR = true;
 
+        [Category("General")]
         [DisplayName("Highlight Color")]
 		[Description("The color to highlight the selected text.")]
 		[DefaultValue(typeof(Color), "Red")]
 		public Color HighlightColor {get; set;}
 
+        [Category("General")]
 		[DisplayName("Highlight Whole Words")]
 		[Description("Only highlights whole words.")]
         [DefaultValue(DEFAULT_WHOLE_WORD)]
 		public bool WholeWords {get; set;}
 
+        [Category("General")]
 		[DisplayName("Match Case")]
 		[Description("Only highlights text with the same case.")]
         [DefaultValue(DEFAULT_MATCH_CASE)]
 		public bool MatchCase {get; set;}
 
+        [Category("General")]
 		[DisplayName("Add Line Marker")]
 		[Description("Adds a line marker next to every highlight.")]
         [DefaultValue(DEFAULT_ADD_LINE_MARKER)]
