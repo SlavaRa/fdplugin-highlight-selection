792f7d90fb7ec077b6ae174f6511c3d757f83862
slavara <ratdg@ya.ru>
2014-05-05 19:22:52 +0400

    Added constants: DEFAULT_WHOLE_WORD, DEFAULT_MATCH_CASE, DEFAULT_ADD_LINE_MARKER

--- Settings.cs
+++ Settings.cs
@@ -7,24 +7,28 @@ namespace HighlightSelection
 	[Serializable]
 	public class Settings
 	{
-		[DisplayName("Highlight Color")]
+        public const bool DEFAULT_WHOLE_WORD = true;
+        public const bool DEFAULT_MATCH_CASE = true;
+        public const bool DEFAULT_ADD_LINE_MARKER = true;
+
+        [DisplayName("Highlight Color")]
 		[Description("The color to highlight the selected text.")]
 		[DefaultValue(typeof(Color), "Red")]
 		public Color HighlightColor {get; set;}
 
 		[DisplayName("Highlight Whole Words")]
 		[Description("Only highlights whole words.")]
-		[DefaultValue(true)]
+        [DefaultValue(DEFAULT_WHOLE_WORD)]
 		public bool WholeWords {get; set;}
 
 		[DisplayName("Match Case")]
 		[Description("Only highlights text with the same case.")]
-		[DefaultValue(true)]
+        [DefaultValue(DEFAULT_MATCH_CASE)]
 		public bool MatchCase {get; set;}
 
 		[DisplayName("Add Line Marker")]
 		[Description("Adds a line marker next to every highlight.")]
-		[DefaultValue(true)]
-		public bool AddLineMarker {get; set;}
+        [DefaultValue(DEFAULT_ADD_LINE_MARKER)]
+        public bool AddLineMarker {get; set;}
 	}
 }
